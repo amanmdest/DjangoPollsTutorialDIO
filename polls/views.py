74d9f746b7685b6d8e62609c133a5a0514c2297d
amanmdest <amanmdest@gmail.com>
2025-09-15 17:11:20 -0300

    Implementando formulário minimalista.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 
 def index(request):
@@ -21,4 +23,20 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/vote.html", {"question": question})
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Você não escolheu uma opção.",
+            },
+        )
+    
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id, )))
