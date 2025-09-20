c9529afdcb92de881de2b6ccdfa5373072cd79e1
amanmdest <amanmdest@gmail.com>
2025-09-20 01:27:15 -0300

    trabalhando com a API de formulários django

---
+++ contacts/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import ContactForm, NameForm
+
+
+def create(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('contacts:thanks', args=("anônimo", )))
+    else:
+        form = ContactForm()
+    return render(request, "contacts/create.html", {"form": form})
+
+
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            return HttpResponseRedirect(reverse('contacts:thanks', args=(name, )))
+    else:
+        form = NameForm()
+
+    return render(request, "contacts/name.html", {"form": form})
+
+
+def thanks(request, name):
+    # request.GET['name']
+    return HttpResponse(f"Congratulations {name}!")
