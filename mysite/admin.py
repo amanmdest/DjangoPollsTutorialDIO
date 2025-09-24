ca9f907dd1ba592c476292ce1d43436c00506c42
amanmdest <amanmdest@gmail.com>
2025-09-24 09:13:08 -0300

    sistemas de autenticação e autorização, admin page

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from polls.models import Choice, Question
-
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django.contrib.auth.models import Group, User
 
 class CustomAdminSite(admin.AdminSite):
     site_header = "Monty Python Administration"
@@ -9,3 +10,5 @@ class CustomAdminSite(admin.AdminSite):
 admin_site = CustomAdminSite()
 admin_site.register(Choice)
 admin_site.register(Question)
+admin_site.register(Group, GroupAdmin)
+admin_site.register(User, UserAdmin)
