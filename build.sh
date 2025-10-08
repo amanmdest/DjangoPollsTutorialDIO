1e7d9ada071c653dc2f9e24ce4d6f9f8c96a7d73
amanmdest <amanmdest@gmail.com>
2025-10-08 09:56:28 -0300

    Criando superuser

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 /#!/usr/bin/env bash
 set -o errexit
 
+# Modify this line as needed for you package manager (pip, poetry, etc.)
 pip install poetry -U
 poetry install --no-root
 
+# Convert static asset files
 python manage.py collectstatic --no-input
 
+# Apply any outstanging database migrations
 python manage.py migrate
+
+# Create superuser
+if [[ $CREATE_SUPERUSER ]];
+then
+    python  manage.py createsuperuser --no-input
